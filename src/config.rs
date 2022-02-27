118864ba9f08650e00d93dc354b7c743e4e0cabe
Brian May <brian@linuxpenguins.xyz>
2022-02-27 13:28:58 +1100

    Add more buttons

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,12 @@
 use crate::button_controllers::lights::LightConfig;
+use crate::button_controllers::music::MusicConfig;
 use crate::button_controllers::switch::SwitchConfig;
 use crate::button_controllers::Action;
 use crate::button_controllers::CommonConfig;
 use crate::button_controllers::Config;
 use crate::button_controllers::Icon;
 
-pub const NUM_CONTROLLERS: usize = 2;
+pub const NUM_CONTROLLERS: usize = 4;
 pub const NIGHT_TOPIC: &str = "state/Brian/Night/power";
 
 pub fn get_controllers_config() -> [Box<dyn Config>; NUM_CONTROLLERS] {
@@ -29,6 +30,27 @@ pub fn get_controllers_config() -> [Box<dyn Config>; NUM_CONTROLLERS] {
                 action: Action::Toggle,
                 icon: Icon::Fan,
             },
+        }),
+        Box::new(LightConfig {
+            c: CommonConfig {
+                name: "Passage".to_string(),
+                location: "Passage".to_string(),
+                device: "Light".to_string(),
+                action: Action::Toggle,
+                icon: Icon::Light,
+            },
+            scene: "default".to_string(),
+            priority: 100,
+        }),
+        Box::new(MusicConfig {
+            c: CommonConfig {
+                name: "WakeUp".to_string(),
+                location: "Brian".to_string(),
+                device: "Robotica".to_string(),
+                action: Action::Toggle,
+                icon: Icon::WakeUp,
+            },
+            play_list: "wake_up".to_string(),
         }),
     ]
 }
