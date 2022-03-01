6786e899b29a9edda6a9eb968851ecb89c93bf75
Brian May <brian@linuxpenguins.xyz>
2022-03-01 19:32:35 +1100

    Simplify touch controller
    Remove stuff that we aren't using.

--- src/touch.rs
+++ src/touch.rs
@@ -1,9 +1,6 @@
-// Adapted from https://github.com/anichno/esp32-touch-sensor-example/blob/a45cd34c43963305bd84fd7dbc31414a5e4c41f4/src/touch.rs
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::{ffi, ptr};
+// Adapted from https://github.com/iamabetterdogtht/esp32-touch-sensor-example/blob/a45cd34c43963305bd84fd7dbc31414a5e4c41f4/src/touch.rs
 
 use anyhow::Result;
-use arr_macro::arr;
 use esp_idf_hal::gpio;
 use esp_idf_sys as sys;
 use esp_idf_sys::esp;
@@ -12,13 +9,10 @@ use embedded_hal::digital::blocking::InputPin;
 use embedded_hal::digital::ErrorType;
 
 const NUM_TOUCH_PINS: usize = 10;
-static TOUCH_PIN_TOUCHED: [AtomicBool; NUM_TOUCH_PINS] = arr![AtomicBool::new(false); 10];
 
 pub struct TouchControllerBuilder {
     touch_pins: [bool; NUM_TOUCH_PINS],
 }
-
-pub struct TouchController {}
 
 pub struct TouchPin {
     pin: sys::touch_pad_t,
@@ -47,23 +41,10 @@ impl TouchControllerBuilder {
         Ok(TouchPin { pin: channel })
     }
 
-    pub fn build(self) -> Result<TouchController> {
+    pub fn build(self) -> Result<()> {
         esp!(unsafe { sys::touch_pad_filter_start(10) })?;
-
-        let mut touch_value = 0;
-        for (i, channel) in self.touch_pins.iter().enumerate() {
-            if *channel {
-                esp!(unsafe { sys::touch_pad_read_filtered(i as _, &mut touch_value) })?;
-                let threshold = touch_value * 2 / 3;
-                esp!(unsafe { sys::touch_pad_set_thresh(i as _, threshold) })?;
-            }
-        }
-
-        esp!(unsafe { sys::touch_pad_isr_register(Some(handle_touch), ptr::null_mut()) })?;
         esp!(unsafe { sys::touch_pad_clear_status() })?;
-        esp!(unsafe { sys::touch_pad_intr_enable() })?;
-
-        Ok(TouchController {})
+        Ok(())
     }
 }
 
@@ -74,27 +55,6 @@ impl TouchPin {
 
         Ok(touch_value)
     }
-
-    // pub fn touched(&self) -> bool {
-    //     let pin = self.pin as usize;
-    //     if TOUCH_PIN_TOUCHED[pin].load(Ordering::SeqCst) {
-    //         TOUCH_PIN_TOUCHED[pin].store(false, Ordering::SeqCst);
-    //         return true;
-    //     }
-
-    //     false
-    // }
-}
-
-unsafe extern "C" fn handle_touch(_: *mut ffi::c_void) {
-    let pad_intr = sys::touch_pad_get_status();
-    if esp!(sys::touch_pad_clear_status()).is_ok() {
-        for (i, tracker) in TOUCH_PIN_TOUCHED.iter().enumerate() {
-            if (pad_intr >> i) & 1 == 1 {
-                tracker.store(true, Ordering::SeqCst);
-            }
-        }
-    }
 }
 
 impl ErrorType for TouchPin {
@@ -103,7 +63,7 @@ impl ErrorType for TouchPin {
 
 impl InputPin for TouchPin {
     fn is_high(&self) -> Result<bool> {
-        Ok(self.read()? > 300)
+        Ok(self.read()? > 400)
     }
 
     fn is_low(&self) -> Result<bool> {
