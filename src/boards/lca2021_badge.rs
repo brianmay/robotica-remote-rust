a533b7869be666555cdcb621a05967f3a56f5105
Brian May <brian@linuxpenguins.xyz>
2022-03-13 08:45:56 +1100

    Disable 2 (annoying) touch buttons

--- src/boards/lca2021_badge.rs
+++ src/boards/lca2021_badge.rs
@@ -35,13 +35,13 @@ pub fn configure_devices(
     let mut touch_builder = TouchControllerBuilder::new().unwrap();
     let touch_pin1 = touch_builder.add_pin(pins.gpio15, 400).unwrap();
     let touch_pin2 = touch_builder.add_pin(pins.gpio12, 400).unwrap();
-    let touch_pin3 = touch_builder.add_pin(pins.gpio27, 400).unwrap();
-    let touch_pin4 = touch_builder.add_pin(pins.gpio14, 400).unwrap();
+    // let touch_pin3 = touch_builder.add_pin(pins.gpio27, 400).unwrap();
+    // let touch_pin4 = touch_builder.add_pin(pins.gpio14, 400).unwrap();
 
     button::touch::configure_touch_button(touch_pin1, tx.clone(), button::ButtonId::PageUp)?;
     button::touch::configure_touch_button(touch_pin2, tx.clone(), button::ButtonId::PageDown)?;
-    button::touch::configure_touch_button(touch_pin3, tx.clone(), button::ButtonId::Controller(0))?;
-    button::touch::configure_touch_button(touch_pin4, tx, button::ButtonId::Controller(1))?;
+    // button::touch::configure_touch_button(touch_pin3, tx.clone(), button::ButtonId::Controller(0))?;
+    // button::touch::configure_touch_button(touch_pin4, tx, button::ButtonId::Controller(1))?;
 
     Ok((Box::new(wifi), display))
 }
